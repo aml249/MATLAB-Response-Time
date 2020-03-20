1f4e33c56654eb64ded4c7f4dd5170e492c4c957
aml249 <49001877+aml249@users.noreply.github.com>
2020-03-20 15:20:28 -0400

    Add files via upload

--- responsetimecalc2.m
+++ responsetimecalc2.m
@@ -22,14 +22,18 @@ data(isnan(data))=0; %replace any NaN values in data array with zeros
 
 %find nearest endpoints in time array to a defined time period (0.4 sec) where data
 %is in initial and final steady states
-iss_ind=interp1(time, 1:length(time),41.025,'nearest'); %find nearest initial steady state time value to 10.8s
-iss_ind_2=interp1(time, 1:length(time),41.425,'nearest'); %find nearest initial steady state time value to 11.3s
+near_iss_time_1=17.6;
+near_iss_time_2=near_iss_time_1+0.4;
+iss_ind=interp1(time, 1:length(time),near_iss_time_1,'nearest'); %find nearest initial steady state time value to 10.8s
+iss_ind_2=interp1(time, 1:length(time),near_iss_time_2,'nearest'); %find nearest initial steady state time value to 11.3s
+iss_freq =mean(data(iss_ind:iss_ind_2)); %take average of frequencies within the initial steady period
 
 %average the initial and final steady state frequencies in data array to
 %get average steady state frequencies
-iss_freq =mean(data(iss_ind:iss_ind_2)); %take average of frequencies within the initial steady period
-fss_ind=interp1(time, 1:length(time),41.55,'nearest'); %find nearest final ss time value to 11.6s
-fss_ind_2=interp1(time, 1:length(time),41.95,'nearest'); %find nearest final ss time value to 12.1s
+near_fss_time_1=18.24;
+near_fss_time_2=near_fss_time_1+0.4;
+fss_ind=interp1(time, 1:length(time),near_fss_time_1,'nearest'); %find nearest final ss time value to 11.6s
+fss_ind_2=interp1(time, 1:length(time),near_fss_time_2,'nearest'); %find nearest final ss time value to 12.1s
 fss_freq=mean(data(fss_ind:fss_ind_2)); %take average of frequencies within the final steady period 
 
 %store frequency values when signal reaches 50,75 & 905% of its steady
@@ -40,14 +44,16 @@ rt_freq3=fss_freq + 0.10*(iss_freq - fss_freq); %freq value when signal reaches 
 
 %define a band of time around the frequency transition of interest to look
 %for times when rt_freq1 - 3 are reached
-[calcband_start, calcband_start_idx]=min(abs(time-41.48)); %find index of start calc time value close to 11 s
-[calcband_stop,calcband_stop_idx]=min(abs(time-41.525));%find index of stop calc time value close to 12 s
+near_calc_start=17.94;
+near_calc_stop=18.06;
+[calcband_start, calcband_start_idx]=min(abs(time-near_calc_start)); %find index of start calc time value close to 11 s
+[calcband_stop,calcband_stop_idx]=min(abs(time-near_calc_stop));%find index of stop calc time value close to 12 s
 dataslice=data(calcband_start_idx:calcband_stop_idx); %slice data to within start and stop band times
 timeslice=time(calcband_start_idx:calcband_stop_idx); %slice time to within start and stop band times
 
 %find start time of transition based on video observation (find nearest to
 %observed time within timeslice)
-[time_start, time_start_index]=min(abs(timeslice-23.66));
+[time_start, time_start_index]=min(abs(timeslice-near_calc_start));
 starttime=timeslice(time_start_index);
 
 %find time when frequency is at rt_freq1 (50% of ss value)
@@ -65,11 +71,13 @@ responsetime75=percent75sstime-starttime;
 percent90sstime=timeslice(data_rt3_idx);
 responsetime90=percent90sstime-starttime;
 
-plot(time(iss_ind:fss_ind_2), data(iss_ind:fss_ind_2))
+plot(time(iss_ind:fss_ind), data(iss_ind:fss_ind))
 y = ylim; % current y-axis limits
 line([percent50sstime percent50sstime],[y(1) y(2)],'Color','red','LineStyle','--')
 line([percent75sstime percent75sstime],[y(1) y(2)],'Color','green','LineStyle','--')
 line([percent90sstime percent90sstime],[y(1) y(2)],'Color','blue','LineStyle','--')
-%plot(percent50sstime,0)
-%xline(percent50sstime,'-',{'50% of ss'});
-%h = vline(percent50sstime,0,'50% of ss')
+line([starttime starttime],[y(1) y(2)],'Color','black','LineStyle','--')
+title('Muscle to Fat Trial 2 Response Time MATLAB Plot')
+xlabel('Time (Seconds)') 
+ylabel('Frequency (Hz)') 
+legend({'Start Time','50% of SS', '75% of SS','90% of SS'},'Location','southeast')
